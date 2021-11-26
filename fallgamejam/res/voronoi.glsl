b559d9c524a5c292bbb55f1fc8afec032d04bf22
Edvard Thörnros <edvard.thornros@gmail.com>
2021-11-26 23:21:00 +0100

    Draw the voronoi

--- fallgamejam/res/voronoi.glsl
+++ fallgamejam/res/voronoi.glsl
@@ -1,5 +1,13 @@
 extern vec2 vertex_positions[20];
+extern int vertex_colors[20];
 extern int num_vertex_positions;
+
+vec4 colors[4] = vec4[](
+    vec4(1., 0., 0., 1.),
+    vec4(0., 1., 0., 1.),
+    vec4(0., 0., 1., 1.),
+    vec4(1., 1., 0., 1.)
+);
 
 float distance_squared(vec2 p1, vec2 p2) {
         vec2 diff = p2 - p1;
@@ -14,7 +22,7 @@ vec4 effect(vec4 color, Image texture, vec2 tc, vec2 pixel_coord) {
                 float dist_sq = distance_squared(pixel_coord, vertex_positions[i]);
                 if (dist_sq < min_dist_sq) {
                         min_dist_sq = dist_sq;
-                        nearest_color = colors[i];
+                        nearest_color = colors[vertex_colors[i]];
                 }
         }
 
